32e7ba5641178e33a4f1a5fe7b12252950609434
Brian J. Cardiff <bcardiff@gmail.com>
2011-09-14 13:57:28 -0300

    remove/simplify nested attribute index thingy. only use new_{assoc} when adding, leave rails the others

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,22 +3,20 @@ module ApplicationHelper
     f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)", options)
   end
   
-  def link_to_edit_fields(name, f, association, edit_object, options={})
-    fields = edit_fields_for f, association, edit_object, options
+  def link_to_edit_fields(name, f, association)
+    fields = edit_fields_for f, association
     
     link_to_function(name, "edit_fields(this, \"#{escape_javascript(fields)}\")", :class => 'farrow')
   end
   
-  def edit_fields_for(f, association, edit_object, options={})
-    f.fields_for(association, edit_object, :child_index => edit_object, :index => options[:index]) do |builder|
-      render(association.to_s.singularize + "_controls", :f => builder)
-    end
+  def edit_fields_for(f, association)
+    render(association.to_s.singularize + "_controls", :f => f)
   end
 
   def link_to_add_fields(name, f, association, options={})
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
-      render(association.to_s.singularize + "_add", :f => builder, :index => "new_#{association}")
+      render(association.to_s.singularize + "_add", :f => builder)
     end
     link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")", options)
   end
