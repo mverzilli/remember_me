9d46f2a6e6fb28fa9eaa61fdbfd0e5b5d7dcc0f7
Brian J. Cardiff <bcardiff@gmail.com>
2011-09-21 15:53:26 -0300

    complete test cases for message edition in fixed scehdules

--- test/unit/fixed_schedule_test.rb
+++ test/unit/fixed_schedule_test.rb
@@ -23,12 +23,16 @@ class FixedScheduleTest < ActiveSupport::TestCase
     Subscriber.subscribe :from => phone, :body => "#{@schedule.keyword} #{offset}", :'x-remindem-user' => @schedule.user.email
   end
 
+  def messages_to(phone)
+    @messages_sent.find_all { |m| m[:to] == phone }
+  end
+
   def assert_no_message_sent(phone)
-    assert !@messages_sent.any? { |m| m[:to] == phone }
+    assert messages_to(phone).empty?, "The following messages were not expected to be sent #{messages_to(phone)}"
   end
   
   def assert_message_sent(phone, text)
-    assert @messages_sent.any? { |m| m[:to] == phone && m[:body] == text }
+    assert messages_to(phone).any? { |m| m[:body] == text }, "#{phone} did not receive expected message: #{text}"
   end
   
   def clear_messages
@@ -115,5 +119,97 @@ class FixedScheduleTest < ActiveSupport::TestCase
     
     time_advance 5.hours
     assert_no_message_sent @phone_1
+  end
+  
+  test "subscribers should receive updated text" do
+    @message1 = @schedule.messages.create! :text => 'text at 1', :offset => 1
+    
+    subscribe @phone_1
+    time_advance 20.hours
+    
+    @message1.update_attributes! :text => 'text at 1 updated'
+    
+    time_advance 5.hours
+    assert_message_sent @phone_1, 'text at 1 updated'
+  end
+  
+  test "subscribers should not receive deleted messages" do
+    @schedule.messages.create! :text => 'text at 1', :offset => 1
+    @message3 = @schedule.messages.create! :text => 'text at 3', :offset => 3
+    
+    subscribe @phone_1
+    time_advance 1.hour
+    time_advance 1.day
+    assert_message_sent @phone_1, 'text at 1'
+    clear_messages
+    
+    time_advance 1.day
+    @message3.delete
+    
+    time_advance 1.day
+    assert_no_message_sent @phone_1
+  end
+
+  test "subscribers should receive created messages after subscription" do
+    @schedule.messages.create! :text => 'text at 1', :offset => 1
+    
+    subscribe @phone_1
+    time_advance 1.hour
+    time_advance 1.day
+    assert_message_sent @phone_1, 'text at 1'
+    clear_messages
+    
+    time_advance 1.day
+    @schedule.messages.create! :text => 'text at 3', :offset => 3
+    
+    time_advance 1.day
+    assert_message_sent @phone_1, 'text at 3'
+  end
+  
+  test "subscribers should not receive created messages after subscription when they have pass that time" do
+    @schedule.messages.create! :text => 'text at 1', :offset => 1
+    
+    subscribe @phone_1
+    time_advance 1.hour
+    time_advance 1.day
+    assert_message_sent @phone_1, 'text at 1'
+    clear_messages
+    
+    time_advance 4.day
+    @schedule.messages.create! :text => 'text at 3', :offset => 3
+    
+    time_advance 1.day
+    assert_no_message_sent @phone_1
+  end
+  
+  test "subscribers should receive later the messages when they are pushed back" do
+    @schedule.messages.create! :text => 'text at 1', :offset => 1
+    
+    subscribe @phone_1
+    time_advance 22.hours
+    
+    @schedule.messages.first.update_attributes :offset => 3
+    time_advance 3.hours
+    assert_no_message_sent @phone_1
+    
+    time_advance 1.day
+    assert_no_message_sent @phone_1
+    
+    time_advance 1.day
+    assert_message_sent @phone_1, 'text at 1'
+  end
+
+  test "subscribers should receive earlier the messages when they are brought forward" do
+    @schedule.messages.create! :text => 'text at 3', :offset => 3
+    
+    subscribe @phone_1
+    time_advance 22.hours
+    
+    @schedule.messages.first.update_attributes :offset => 2
+    time_advance 3.hours
+    assert_no_message_sent @phone_1
+    
+    time_advance 1.day
+    assert_message_sent @phone_1, 'text at 3'
   end
 end
