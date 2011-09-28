df0dbbb9e46cf328de79f7396ffdb7f0542ea7a6
nekron <marianoabelcoca@gmail.com>
2011-09-28 13:03:54 -0300

    Fixed log sorting between pages.
    Still needs performance testing on a loaded system. This is just a
    workaround...

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -11,7 +11,9 @@ class LogsController < AuthenticatedController
   def index
     add_breadcrumb Schedule.find(params[:schedule_id]).title, schedule_path(params[:schedule_id])
 	  add_breadcrumb "Logs", schedule_logs_path(params[:schedule_id])
-    @logs = Log.where(:schedule_id => params[:schedule_id]).page(params[:page]).per(50)
+	  
+	  @logs = Log.where(:schedule_id => params[:schedule_id]).sort_by(&:created_at).reverse
+    @logs = Kaminari.paginate_array(@logs).page(params[:page]).per(50)
 
     respond_to do |format|
       format.html # index.html.erb
