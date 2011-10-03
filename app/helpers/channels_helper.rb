cd017087493bd0fa389d3bdac8b1af9adbc7095b
nekron <marianoabelcoca@gmail.com>
2011-10-03 19:00:40 -0300

    Local Gateway Configuration Wizard Styled

--- app/helpers/channels_helper.rb
+++ app/helpers/channels_helper.rb
@@ -1,2 +1,36 @@
 module ChannelsHelper
+
+  def partial_from params
+    partial = 'user_channel'
+
+    if params[:create_from_android]
+    		partial = 'new_android_channel'
+  	end 
+
+    if params[:create_from_desktop]
+    		partial = 'new_desktop_channel'
+  	end
+
+    if params[:show_desktop_local_gateway] 
+    		partial = 'download_desktop_local_gateway'
+   	end
+
+    if params[:show_android_local_gateway]
+    		partial = 'download_android_local_gateway'
+  	end
+
+    if params[:choose_local_gateway]
+    		partial = 'choose_local_gateway'
+  	end
+
+    if params[:show_local_gateway]
+    		partial = 'choose_local_gateway'
+  	end
+
+    if params[:show_current]
+    		partial = 'user_channel'
+  	end
+  	
+  	partial
+	end
 end
